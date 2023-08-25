4a39afa74e66cedabd04091bd86c86077f60fc01
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2023-08-25 13:27:24 +0300

    durka

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 import axios from 'axios';
 import simpleLightbox from 'simplelightbox';
-import Notiflix from 'notiflix';
+import { htmlElements, error, goodSearch, createCards } from './js/helpers';
 
-function error(err) {
-  Notiflix.Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.'
-  );
-}
-
-const htmlElements = {
-  form: document.querySelector('.search-form'),
-  div: document.querySelector('.gallery'),
-  btnLoadMore: document.querySelector('.voltage-button'),
-  sorry: document.querySelector('.sorry'),
-};
-
-let numbPage = 0;
 let searchValue = '';
+let numbPage = 0;
 let stopCounter = 0;
 
+function returnStartValue() {
+  htmlElements.btnLoadMore.style.display = 'none';
+  htmlElements.sorry.style.display = 'none';
+  htmlElements.div.innerHTML = '';
+  numbPage = 0;
+  stopCounter = 0;
+}
 function createSearch(search) {
   const BASE_URL = 'https://pixabay.com/api/?';
   const searchParams = new URLSearchParams({
@@ -30,7 +24,6 @@ function createSearch(search) {
     page: (numbPage += 1),
     per_page: 40,
   });
-console.log(`${BASE_URL}${searchParams}`);
   return axios
     .get(`${BASE_URL}${searchParams}`)
     .then(res => {
@@ -39,43 +32,9 @@ console.log(`${BASE_URL}${searchParams}`);
       }
       return res.data;
     })
-    .catch(() => {
-      error();
+    .catch(err => {
+      console.log(err);
     });
-}
-
-function createCards(arr) {
-  return arr
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<div class="photo-card">
-    <img class='img-card' src="${webformatURL}" alt="${tags}" loading="lazy" width='300px' height='200px'/>
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>${likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b>${views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b>${comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>${downloads}
-      </p>
-    </div>
-  </div>`;
-      }
-    )
-    .join('');
 }
 
 htmlElements.form.addEventListener('submit', searchFn);
@@ -83,19 +42,20 @@ htmlElements.btnLoadMore.addEventListener('click', loadMoreFn);
 
 function searchFn(evt) {
   evt.preventDefault();
-  htmlElements.btnLoadMore.style.display = 'none';
-  htmlElements.div.innerHTML = '';
-  numbPage = 0;
-  stopCounter = 0;
+  returnStartValue();
+
   searchValue = evt.target.searchQuery.value;
   createSearch(searchValue)
     .then(res => {
       if (res) {
         htmlElements.div.insertAdjacentHTML('beforeend', createCards(res.hits));
         stopCounter += 40;
+        goodSearch(res.totalHits);
         if (res.totalHits >= stopCounter) {
           htmlElements.btnLoadMore.style.display = 'block';
+          return;
         }
+        htmlElements.sorry.style.display = 'inline-block';
       }
     })
     .catch(err => {
@@ -107,8 +67,6 @@ function loadMoreFn() {
 
   createSearch(searchValue)
     .then(res => {
-      console.log(res);
-      console.log(stopCounter);
       if (!res) {
         throw error;
       }
